140b2215a9fb78d06dc176e563ef131046df2f37
TIFFANY <tiffanyliu2008@yahoo.com>
2023-11-04 13:03:55 -0700

    Sat 1104 AuthMe Frontend

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -7,7 +7,7 @@ const setUser = (user) => {
     return {
         type: SET_USER,
         payload: user,
-    } ;
+    };
 };
 
 const removeUser = () => {
@@ -71,5 +71,13 @@ export const signup = (user) => async (dispatch) => {
     });
     const data = await response.json();
     dispatch(setUser(data.user));
+    return response;
+};
+
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
     return response;
 };
